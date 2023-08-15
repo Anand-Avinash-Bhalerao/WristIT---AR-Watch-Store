8495caa45a25b81e90507716c7651d615ee5c90b
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-08-15 19:14:48 +0530

    Changes in the UI of watch info activity

--- app/src/main/java/com/billion_dollor_company/accessorystore/models/WatchInfo.kt
+++ app/src/main/java/com/billion_dollor_company/accessorystore/models/WatchInfo.kt
@@ -1,3 +1,14 @@
-package com.billion_dollor_company.accessorystore.models
 
-data class WatchInfo(val company:String,val name:String, val description:String, val price:Int,val discount:Int,val imageURL:String)
+package com.billion_dollor_company.accessorystore.models
+import android.os.Parcel
+import android.os.Parcelable
+import java.io.Serializable
+
+
+data class WatchInfo(
+    val company: String,
+    val name:String, val description:String,
+    val price:Int,
+    val discount:Int,
+    val imageURL:String,
+    val type: String)
